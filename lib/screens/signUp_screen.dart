3b28e6acc56b9b67dd2dc4c7bf16844246900404
Joel Wanjohi <joelwanjohi207@gmail.com>
2024-02-28 23:51:46 +0300

    m

--- lib/screens/signUp_screen.dart
+++ lib/screens/signUp_screen.dart
@@ -38,124 +38,122 @@ class _SignUpState extends State<SignUp> {
               resizeToAvoidBottomInset: false,
       
               backgroundColor: Colors.transparent,
-              body: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Flexible(
-                      child: Center(
-                        child: Text(
-                          'Dreamscape Properties',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold),
-                        ),
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Flexible(
+                    child: Center(
+                      child: Text(
+                        'Dreamscape Properties',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color: Colors.green[400],
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: const Center(
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                prefixIcon: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 20),
-                                  child: Icon(
-                                    Icons.mail,
-                                    color: Color.fromARGB(255, 47, 5, 64),
-                                  ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.green[400],
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Icon(
+                                  Icons.mail,
+                                  color: Color.fromARGB(255, 47, 5, 64),
                                 ),
-                                hintText: 'Enter your Email',
-                                hintStyle: TextStyle(color: Colors.white)),
-                            keyboardType: TextInputType.emailAddress,
-                            textInputAction: TextInputAction.next,
-                          ),
+                              ),
+                              hintText: 'Enter your Email',
+                              hintStyle: TextStyle(color: Colors.white)),
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color: Colors.green[400],
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: const Center(
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                prefixIcon: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 20),
-                                  child: Icon(
-                                    Icons.lock,
-                                    color: Color.fromARGB(255, 47, 5, 64),
-                                  ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.green[400],
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Icon(
+                                  Icons.lock,
+                                  color: Color.fromARGB(255, 47, 5, 64),
                                 ),
-                                hintText: 'Enter your Password',
-                                hintStyle: TextStyle(color: Colors.white)),
-                            keyboardType: TextInputType.name,
-                            textInputAction: TextInputAction.done,
-                            obscureText: true,
-                          ),
+                              ),
+                              hintText: 'Enter your Password',
+                              hintStyle: TextStyle(color: Colors.white)),
+                          keyboardType: TextInputType.name,
+                          textInputAction: TextInputAction.done,
+                          obscureText: true,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color: Colors.green[400],
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: const Center(
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                prefixIcon: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 20),
-                                  child: Icon(
-                                    Icons.lock,
-                                    color: Color.fromARGB(255, 47, 5, 64),
-                                  ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.green[400],
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Icon(
+                                  Icons.lock,
+                                  color: Color.fromARGB(255, 47, 5, 64),
                                 ),
-                                hintText: 'Confirm Password',
-                                hintStyle: TextStyle(color: Colors.white)),
-                            keyboardType: TextInputType.emailAddress,
-                            textInputAction: TextInputAction.next,
-                          ),
+                              ),
+                              hintText: 'Confirm Password',
+                              hintStyle: TextStyle(color: Colors.white)),
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
                         ),
                       ),
                     ),
-                    Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Container(
-                          height: 40,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: const Color.fromARGB(255, 5, 64, 21)),
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => LoginScreen()));
-                            },
-                            child: Text('Register',
-                                style: TextStyle(color: Colors.white)),
-                          ),
-                        )),
-                  ],
-                ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        height: 40,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: const Color.fromARGB(255, 5, 64, 21)),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginScreen()));
+                          },
+                          child: Text('Register',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      )),
+                ],
               ),
             )
           ],
